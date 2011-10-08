c53e1faff3016e4efc92f90324fc8589aeeb41f6
Xavier Del Castillo <xdelcastillo@archlinux.us>
2011-10-08 00:43:29 -0700

    Created main start up script, this is temporary, but it works perfectly for one page depth, please see main.py comments for more info

--- src/main.py
+++ src/main.py
@@ -1,30 +1,88 @@
 #
 # Main executable
 #
-import mechanize
-from lib import chuwlib
+
+from chuwler import chuwlib
+from lxml import html
+
+setParsedLinks = set()
 
 class startScrap():
-    def __init__(self):
 
-        self.browser = mechanize.Browser()
+    def __init__(self,baseurl):
+        self.baseurl = baseurl
+        pass
 
-    def scrapwl(self,url):
-        browse = self.browser
-        browse.open(url)
+    def parseLinks(self,newLinks):
+        """This method cleans the set of links read from the current page
+        and compares it with the global set that contains the checked links
+        it returns the links that haven't been checked yet"""
+        
+        global setParsedLinks
+
+        return newLinks.difference(setParsedLinks)
+
+    def retrieveLinksFromHTML(self,raw_html):
+        """This method retrieves the links from the current page, it filters
+        all the links on the page and only return these that are either root
+        relative or have exactly the same root"""
+
+        setUniqueLinks = set()
+        
+        rootRelativeLinks = raw_html.xpath("//a[starts-with(@href,'/')]/@href")
+        for index,hrefValue in enumerate(rootRelativeLinks):
+            rootRelativeLinks[index] = self.baseurl+hrefValue
+
+        LinksWithBaseURL = raw_html.xpath("//a[starts-with(@href,'"+self.baseurl+"')]/@href")
+
+        setUniqueLinks = set(rootRelativeLinks)
+        setUniqueLinks = setUniqueLinks.union(set(LinksWithBaseURL))
+
+        return setUniqueLinks
+
+    def evaluateLinks(self,setLinksEvaluate):
+        """Evaluates each link given from the argument, the argument is a set
+        variable. Everytime it checks a link, it's added on the global set of
+        checked links."""
+
+        global setParsedLinks
+
         navigate = chuwlib.createOpener()
-        navigate.urlopen(url)
 
-        for links in browse.links(url_regex="http|https"):
-            print "Now checking %s" % links.url
+        for link in setLinksEvaluate:
+            print "Now checking %s" % link
             try:
-                navigate.urlopen(links.url)
+                navigate.urlopen(link)
+                setParsedLinks.add(link)
             except:
+                # I need proper exception handling here
                 print "Some exception found"
-                pass
+        pass
+
+    def scrapwl(self):
+        """Main function to initiate the process, it starts by
+        reading the URL and going through all the methods above.
+        This need to be further developed, modified, or even removed
+        this is intended to be a temporary main method for testing
+        the program itself"""
+
+        global setParsedLinks
+
+        url = self.baseurl
+
+        navigate = chuwlib.createOpener()
+        renderPage = navigate.urlopen(url)
+        HTMLparse = html.parse(renderPage)
+
+        setLinksForCheck = self.retrieveLinksFromHTML(HTMLparse)
+        print str(len(setLinksForCheck)) + " Links found."
+        setLinksForCheck = self.parseLinks(setLinksForCheck)
+        print str(len(setLinksForCheck)) + " UNIQUE Links found."
+        self.evaluateLinks(setLinksForCheck)
+
+        pass
 
 if __name__ == '__main__':
-
-    x = startScrap()
-    x.scrapwl("http://www.afrigeneas.com/links/")
-
+    url = "http://www.afrigeneas.com"
+    x = startScrap(url)
+    x.scrapwl()
